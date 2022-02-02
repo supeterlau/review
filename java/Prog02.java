9c9761082cbfd7857b573bddf337830f569ec886
Peter Lau <superpeterlau@outlook.com>
2022-02-02 15:21:25 +0800

    Basic Java 02

---
+++ java/Prog02.java
@@ -0,0 +1,10 @@
+public class Prog02 {
+	public Prog02(String name) {
+		System.out.println("Passed name is " + name);
+	}
+
+	public static void main(String args[]) {
+		Prog02 puppy = new Prog02("tommy");
+	}
+}
+
