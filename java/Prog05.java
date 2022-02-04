d0e81760e56e38244d2537eaa12e33143650ff96
Peter Lau <superpeterlau@outlook.com>
2022-02-04 10:27:45 +0800

    Basic Java 05

---
+++ java/Prog05.java
@@ -0,0 +1,9 @@
+public class Prog05 {
+	private static double salary;
+	private static final String ENV = "Development";
+
+	public static void main(String args[]) {
+		salary = 1000;
+		System.out.println(ENV + " average salary: " + salary);
+	}
+}
