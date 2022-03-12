038e0fc78f1de58b7f9b1ed1e9da4aa1fd792a84
Peter Lau <superpeterlau@outlook.com>
2022-03-13 07:22:28 +0800

    scala implicit object

---
+++ scala/Prog04_Run.scala
@@ -0,0 +1,12 @@
+object Prog04Run {
+  implicit class IntTimes(x: Int) {
+    def times [A](f: => A): Unit = {
+      def loop(current: Int): Unit = 
+        if(current > 0) {
+          f
+          loop(current - 1)
+        }
+        loop(x)
+    }
+  }
+}
